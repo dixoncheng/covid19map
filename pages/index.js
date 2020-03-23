4691c913ac55d846adeaaa099e4519a58d59e14f
Dixon Cheng <mail@dixoncheng.com>
2020-03-23 20:17:50 +1300

    Moved scraper into separate file

--- pages/index.js
+++ pages/index.js
@@ -2,14 +2,8 @@ import Head from "next/head";
 import dynamic from "next/dynamic";
 import { useState } from "react";
 import styled, { css } from "styled-components";
-import cheerio from "cheerio";
-import locations from "../constants/locations";
-// import mohHtml from "../moh-html";
 
-const fetch = require("@zeit/fetch-retry")(require("node-fetch"));
-
-const URL =
-  "https://www.health.govt.nz/our-work/diseases-and-conditions/covid-19-novel-coronavirus/covid-19-current-cases";
+import scraper from "../scraper";
 
 const totalCases = 102;
 
@@ -217,90 +211,7 @@ const Home = ({ data, lastUpdated }) => {
 export default Home;
 
 export async function getStaticProps() {
-  const response = await fetch(URL);
-  const html = await response.text();
-  // const html = mohHtml;
-
-  const $ = await cheerio.load(html);
-
-  const lastUpdated = $(".page_updated .date").text();
-
-  let cases = [];
-  $(".table-style-two tbody tr").each((i, elem) => {
-    cases.push({
-      caseId: $(elem)
-        .find("td:nth-child(1)")
-        .text()
-        .trim(),
-      location: $(elem)
-        .find("td:nth-child(2)")
-        .text()
-        .trim(),
-      age: $(elem)
-        .find("td:nth-child(3)")
-        .text()
-        .trim(),
-      gender: $(elem)
-        .find("td:nth-child(4)")
-        .text()
-        .trim(),
-      details: $(elem)
-        .find("td:nth-child(5)")
-        .text()
-        .trim()
-    });
-  });
-
-  let data = [];
-  let totalCases = 0;
-  cases.forEach(item => {
-    if (item.location) {
-      totalCases++;
-
-      // correct typos on MOH site
-      if (item.location === "Coramandel") {
-        item.location = "Coromandel";
-      }
-      if (item.location === "Dundedin") {
-        item.location = "Dunedin";
-      }
-
-      // normalize genders
-      if (item.gender === "M") {
-        item.gender = "Male";
-      }
-      if (item.gender === "F") {
-        item.gender = "Female";
-      }
-
-      const n = data.find(x => item.location === x.location);
-      if (n) {
-        n.numCases++;
-        n.cases.push(item);
-      } else {
-        const loc = locations.find(x => item.location === x.location);
-        if (loc) {
-          data.push({
-            location: item.location,
-            numCases: 1,
-            latlng: loc.latlng,
-            cases: [item]
-          });
-        } else {
-          // region doesn't exist in constants
-          throw new Error(`No location "${item.location}" exist`);
-        }
-      }
-    }
-  });
-
-  data.sort((a, b) => {
-    if (a.numCases === b.numCases) {
-      return a.location > b.location ? 1 : -1;
-    }
-    return a.numCases > b.numCases ? -1 : 1;
-  });
-
+  const { data, lastUpdated, totalCases } = await scraper();
   return {
     props: {
       data,
---
+++ scraper.js
@@ -0,0 +1,98 @@
+import cheerio from "cheerio";
+import locations from "./constants/locations";
+// import mohHtml from "./moh-html";
+
+const fetch = require("@zeit/fetch-retry")(require("node-fetch"));
+
+const URL =
+  "https://www.health.govt.nz/our-work/diseases-and-conditions/covid-19-novel-coronavirus/covid-19-current-cases";
+
+const scraper = async () => {
+  const response = await fetch(URL);
+  const html = await response.text();
+  // const html = mohHtml;
+
+  const $ = await cheerio.load(html);
+
+  const lastUpdated = $(".page_updated .date").text();
+
+  let cases = [];
+  $(".table-style-two tbody tr").each((i, elem) => {
+    cases.push({
+      caseId: $(elem)
+        .find("td:nth-child(1)")
+        .text()
+        .trim(),
+      location: $(elem)
+        .find("td:nth-child(2)")
+        .text()
+        .trim(),
+      age: $(elem)
+        .find("td:nth-child(3)")
+        .text()
+        .trim(),
+      gender: $(elem)
+        .find("td:nth-child(4)")
+        .text()
+        .trim(),
+      details: $(elem)
+        .find("td:nth-child(5)")
+        .text()
+        .trim()
+    });
+  });
+
+  let data = [];
+  let totalCases = 0;
+  cases.forEach(item => {
+    if (item.location) {
+      totalCases++;
+
+      // correct typos on MOH site
+      if (item.location === "Coramandel") {
+        item.location = "Coromandel";
+      }
+      if (item.location === "Dundedin") {
+        item.location = "Dunedin";
+      }
+
+      // normalize genders
+      if (item.gender === "M") {
+        item.gender = "Male";
+      }
+      if (item.gender === "F") {
+        item.gender = "Female";
+      }
+
+      const n = data.find(x => item.location === x.location);
+      if (n) {
+        n.numCases++;
+        n.cases.push(item);
+      } else {
+        const loc = locations.find(x => item.location === x.location);
+        if (loc) {
+          data.push({
+            location: item.location,
+            numCases: 1,
+            latlng: loc.latlng,
+            cases: [item]
+          });
+        } else {
+          // region doesn't exist in constants
+          throw new Error(`No location "${item.location}" exist`);
+        }
+      }
+    }
+  });
+
+  data.sort((a, b) => {
+    if (a.numCases === b.numCases) {
+      return a.location > b.location ? 1 : -1;
+    }
+    return a.numCases > b.numCases ? -1 : 1;
+  });
+
+  return { data, lastUpdated, totalCases };
+};
+
+export default scraper;
