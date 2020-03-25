7f12b8285a8788af8bd3b413731ba2ef3fd0bdbf
Dixon Cheng <mail@dixoncheng.com>
2020-03-26 10:00:57 +1300

    Font fix

--- pages/_document.js
+++ pages/_document.js
@@ -32,9 +32,9 @@ export default class MyDocument extends Document {
         <Head>
           <link rel="icon" href="/favicon.ico" />
           <link
-            href="https://fonts.googleapis.com/css2?family=Bowlby+One&Nunito:wght@400;700&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;700&family=Bowlby+One&display=swap"
             rel="stylesheet"
-          ></link>
+          />
           <link
             rel="stylesheet"
             href="https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.1/leaflet.css"
