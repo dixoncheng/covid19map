1e15242962cd9231091a6ae14648295dd363f5db
Dixon Cheng <mail@dixoncheng.com>
2020-04-07 15:07:15 +1200

    Disable pwa caching for now

--- next.config.js
+++ next.config.js
@@ -1,11 +1,14 @@
 const withOptimizedImages = require("next-optimized-images");
 const withPWA = require("next-pwa");
+// const defaultCache = require("next-pwa/cache");
+// defaultCache.splice(4, 1);
 
 module.exports = withOptimizedImages(
   withPWA({
     pwa: {
       dest: "public",
-      disable: process.env.ENV === "development"
-    }
+      disable: process.env.ENV === "development",
+      runtimeCaching: [],
+    },
   })
 );
