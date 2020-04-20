458809bf8823dc739ccac4b7995801a6dcc568f5
Dixon Cheng <mail@dixoncheng.com>
2020-04-20 23:42:00 +1200

    Stats page redirect

--- pages/stats.js
+++ pages/stats.js
@@ -1,33 +1,10 @@
-import { render } from "react-dom";
-import React, { useState } from "react";
-import { useSpring, animated } from "react-spring";
-import useMeasure from "../components/useMeasure";
+import { useEffect } from "react";
 
-const Anim = () => {
-  const [open, toggle] = useState(false);
-  const [bind, { width }] = useMeasure();
-  const props = useSpring({
-    width: open ? width : 0,
-    onRest() {
-      console.log("onRest");
-    },
+const Stats = () => {
+  useEffect(() => {
+    window.location = "/";
   });
-
-  return (
-    <div {...bind} class="main" onClick={() => toggle(!open)}>
-      <animated.div className="fill" style={props} />
-      <animated.div className="content">
-        {props.width.interpolate((x) => x.toFixed(0))}
-      </animated.div>
-    </div>
-  );
+  return <div />;
 };
 
-const Test = () => (
-  <div>
-    <Anim />
-    <Anim />
-  </div>
-);
-
-export default Test;
+export default Stats;
